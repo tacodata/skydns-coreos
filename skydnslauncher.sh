b50e167aa51573f1efff0386a1b0a59f39c9780b
Greg Fausak <lgfausak@gmail.com>
2015-03-03 12:10:28 -0600

    more little changes

--- skydnslauncher.sh
+++ skydnslauncher.sh
@@ -7,9 +7,8 @@
 if [ -z "$ETCD_HOSTS" ]; then
     docker_host=$(ip route | grep default | cut -d' ' -f3)
     >&2 echo "set ETCD_MACHINES to ${docker_host}"
-    my_host=$(/sbin/ifconfig eth0 | grep 'inet ' | awk '{print $2'} | sed 's/addr://')
+    my_host=$(grep $(hostname) /etc/hosts | awk '{print $1}')
     >&2 echo "set SKYDNS_ADDR to ${my_host}"
-    >&2 /sbin/ifconfig
     export ETCD_MACHINES="http://${docker_host}:4001"
     export SKYDNS_ADDR="${my_host}:53"
 fi
